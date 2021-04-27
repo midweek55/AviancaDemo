9ca9287a8d24e9b81ced3cf70b0583ade422ffe9
midweek <angelo_mpm15@hotmail.com>
2021-04-27 13:24:50 -0500

    FC2

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -10,11 +10,6 @@ public class App
 {
     public static void main( String[] args ) 
     {
-        try {
-             System.out.println(poi.a().size());
-        } catch (Exception e) {
-            System.out.println(e);
-        }
         
     }
 }
